df19a30ad1efe97a2e40e0587f92c2b2eb95c1d2
tyru <tyru.exe@gmail.com>
2013-08-12 20:37:59 +0900

    initial import

---
+++ config.js
@@ -0,0 +1,150 @@
+
+
+var SAVEDATA_DIR = "%USERPROFILE%\\Dropbox\\東方savedata"
+var PROG_INFO = {
+    '東方紅魔郷': {
+        'dir': 'C:\\Program Files\\東方紅魔郷'
+        , 'replay': {
+            'type': 'dir'
+            , 'name': 'replay'
+        }
+        , 'score': {
+            'type': 'file'
+            , 'name': 'score.dat'
+        }
+        , 'config': {
+            'type': 'file'
+            , 'name': '東方紅魔郷.cfg'
+        }
+    }
+    , '東方妖々夢': {
+        'dir': 'C:\\Program Files (x86)\\東方妖々夢'
+        , 'replay': {
+            'type': 'dir'
+            , 'name': 'replay'
+        }
+        , 'score': {
+            'type': 'file'
+            , 'name': 'score.dat'
+        }
+        , 'config': {
+            'type': 'file'
+            , 'name': 'th07.cfg'
+        }
+    }
+    , '東方永夜抄': {
+        'dir': 'C:\\Program Files (x86)\\東方永夜抄'
+        , 'replay': {
+            'type': 'dir'
+            , 'name': 'replay'
+        }
+        , 'score': {
+            'type': 'file'
+            , 'name': 'score.dat'
+        }
+        , 'config': {
+            'type': 'file'
+            , 'name': 'th08.cfg'
+        }
+    }
+    , '東方花映塚': {
+        'dir': 'C:\\Program Files (x86)\\東方花映塚'
+        , 'replay': {
+            'type': 'dir'
+            , 'name': 'replay'
+        }
+        , 'score': {
+            'type': 'file'
+            , 'name': 'score.dat'
+        }
+        , 'config': {
+            'type': 'file'
+            , 'name': 'th09.cfg'
+        }
+    }
+    , '東方文花帖': {
+        'dir': 'C:\\Program Files (x86)\\東方文花帖'
+        , 'replay': {
+            'type': 'dir'
+            , 'name': 'replay'
+        }
+        , 'score': {
+            'type': 'file'
+            , 'name': 'scoreth095.dat'
+        }
+        , 'config': {
+            'type': 'file'
+            , 'name': 'th095.cfg'
+        }
+        , 'bestshot': {
+            'type': 'dir'
+            , 'name': 'bestshot'
+        }
+    }
+    , '東方風神録': {
+        'dir': 'C:\\Program Files (x86)\\上海アリス幻樂団\\東方風神録'
+        , 'replay': {
+            'type': 'dir'
+            , 'name': 'replay'
+        }
+        , 'score': {
+            'type': 'file'
+            , 'name': 'score.dat'
+        }
+        , 'config': {
+            'type': 'file'
+            , 'name': 'th10.cfg'
+        }
+    }
+    , '東方地霊殿': {
+        'dir': 'C:\\Program Files (x86)\\上海アリス幻樂団\\東方地霊殿'
+        , 'replay': {
+            'type': 'dir'
+            , 'name': 'replay'
+        }
+        , 'score': {
+            'type': 'file'
+            , 'name': 'scoreth11.dat'
+        }
+        , 'config': {
+            'type': 'file'
+            , 'name': 'th11.cfg'
+        }
+    }
+    , '東方星蓮船': {
+        'dir': 'C:\\Program Files (x86)\\上海アリス幻樂団\\東方星蓮船'
+        , 'replay': {
+            'type': 'dir'
+            , 'name': 'replay'
+        }
+        , 'score': {
+            'type': 'file'
+            , 'name': 'scoreth12.dat'
+        }
+        , 'config': {
+            'type': 'file'
+            , 'name': 'th12.cfg'
+        }
+    }
+    , 'ダブルスポイラー': {
+        'dir': 'C:\\Program Files (x86)\\上海アリス幻樂団\\ダブルスポイラー'
+        , 'datadir': {
+            'type': 'dir'
+            , 'name': '%APPDATA%\\ShanghaiAlice\\th125'
+        }
+    }
+    , '妖精大戦争': {
+        'dir': 'C:\\Program Files (x86)\\上海アリス幻樂団\\妖精大戦争'
+        , 'datadir': {
+            'type': 'dir'
+            , 'name': '%APPDATA%\\ShanghaiAlice\\th128'
+        }
+    }
+    , '東方神霊廟': {
+        'dir': 'C:\\Program Files (x86)\\上海アリス幻樂団\\東方神霊廟'
+        , 'datadir': {
+            'type': 'dir'
+            , 'name': '%APPDATA%\\ShanghaiAlice\\th13'
+        }
+    }
+};
